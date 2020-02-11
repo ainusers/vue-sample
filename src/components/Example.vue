0e890d00685765cbcfc87fb258da8093f422fb29
ainusers <979111986@qq.com>
2020-02-11 14:20:58 +0800

    vue-sample init

---
+++ src/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+</template>
+ 
+<script>
+  import {Button} from 'vant'
+  export default {
+    name: 'VantComponent',
+    components:{
+      [Button.name]: Button
+    }
+  }
+</script>
